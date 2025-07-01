19a5d3fda281f0be594f50fdff7bb822ec7f2285
Robert.Artenie <robert.artenie@aelno.com>
2025-07-01 09:52:07 +0000

    Fix runner

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/rah-0/margo/conf"
 	"github.com/rah-0/margo/db"
+	"github.com/rah-0/margo/template"
 )
 
 func main() {
@@ -17,4 +18,26 @@ func main() {
 	}
 	defer conn.Close()
 
+	tableNames, err := db.GetDbTables(conn)
+	if err != nil {
+		nabu.FromError(err).WithLevelFatal().Log()
+		return
+	}
+
+	for _, tn := range tableNames {
+		tfs, err := db.GetDbTableFields(conn, tn)
+		if err != nil {
+			nabu.FromError(err).WithLevelFatal().Log()
+			return
+		}
+		if err := template.CreateGoFileEntity(tn, tfs); err != nil {
+			nabu.FromError(err).WithLevelFatal().Log()
+			return
+		}
+	}
+
+	if err := template.CreateGoFileQueries(); err != nil {
+		nabu.FromError(err).WithLevelFatal().Log()
+		return
+	}
 }
